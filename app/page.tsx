a2ffc72e44cbe14f09de839cddb023687af3bef0
Samarth Kulkarni <samarth.kulkarni@joshsoftware.com>
2025-05-25 21:19:43 +0530

    [refactor]: replace hardcoded navbar items with NavItems component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
-import Navbar from '@/components/Navbar'
 
 const Page = () => {
   return (
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,20 @@
-import Image from "next/image";
-import Link from "next/link";
-import logo from "@/public/images/logo.png";
+import Image from 'next/image'
+import Link from 'next/link'
+import logo from '@/public/images/logo.png'
+import NavItems from './NavItems'
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <Link href="/">
-        <div className="flex items-center gap-2.5 cursor-pointer">
-          <Image src={logo} alt="Edu-Nexus Logo" width={46} height={44} />
+    <nav className='navbar'>
+      <Link href='/'>
+        <div className='flex items-center gap-2.5 cursor-pointer'>
+          <Image src={logo} alt='Edu-Nexus Logo' width={46} height={44} />
         </div>
       </Link>
 
-      <div className="flex items-center gap-8">
-        <p>Home</p>
-        <p>Companions </p>
-        <p>My Journey </p>
-      </div>
+      <NavItems />
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
