266e3f6e98ef21fd11226c68bb2c10c1e0a941f3
Samarth Kulkarni <samarth.kulkarni@joshsoftware.com>
2025-05-25 20:58:29 +0530

    [add]: configure ESLint and Prettier, update base color in components.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
-import React from "react";
-import { Button } from "@/components/ui/button";
-import Navbar from "@/components/Navbar";
+import React from 'react'
+import { Button } from '@/components/ui/button'
+import Navbar from '@/components/Navbar'
 
 const Page = () => {
   return (
     <>
-      <div className="flex space-x-4">
-        <h1 className={"text-2xl underline animate-[fade-in_0.3s_ease-in-out]"}>
-          Welcome to EduNexus
-        </h1>
-        <Button className={"btn-primary "}>Let&#39;s get started</Button>
+      <div className='flex space-x-4'>
+        <h1 className={'text-2xl underline animate-[fade-in_0.3s_ease-in-out]'}>Welcome to EduNexus</h1>
+        <Button className={'btn-primary '}>Let&#39;s get started</Button>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
